238629b9e91324a7d55521ae4f783dc707deb259
Samuel Cheng <samuelcheng@college.harvard.edu>
2016-11-15 02:56:02 +0000

    extracted fruits and veggies data from xlsx spreadsheets, created fruits_veggies.json

--- data_wrangling/usda_fruits_veggies_prices.py
+++ data_wrangling/usda_fruits_veggies_prices.py
@@ -3,24 +3,15 @@ from os import listdir
 from os.path import isfile, join
 import pandas as pd
 import re
+import json
 
 from IPython import embed
 
-curr_path = os.path.realpath(__file__)
+curr_path = os.path.dirname(os.path.realpath(__file__))
 
-embed()
-mypath = join(curr_path, "../../..")
+mypath = os.path.abspath(join(curr_path, "..", "data_raw", "fruits_veggies"))
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-file_name="apricots.xlsx"
-prod_name = file_name[-5:]
-df = pd.read_excel(file_name, header=1)
-df.dropna(axis=0, how='any', inplace=True)
-
-empty_cols = [df.columns[2], df.columns[5]]
-df = df[[c for c in df.columns if c not in empty_cols]]
-df.columns = ["form", "price_pound", "yield", "cup_equiv", "price_cup"]
 
 def condense_form_label(label):
 	return {
@@ -33,10 +24,32 @@ def condense_form_label(label):
 # remove non-alphanumeric
 pattern = re.compile('[\W_]+')
 
-df.loc[:, "form"] = df.form.apply(lambda x: ''.join([c for c in str(x) if not c.isdigit()]))
-df.loc[:, "form"] = df.form.apply(lambda x: condense_form_label(pattern.sub('', x)))
+final_data = []
+for file_name in onlyfiles:
+	file_path = os.path.join(mypath, file_name)
+	prod_name = file_name[:-5]
+	print(file_name)
 
-{
-	"name": prod_name, 
-	"data": df.to_dict(orient='records')
-}
+	df = pd.read_excel(file_path, header=1)
+	# only the first 7 columns have useful data
+	df = df[df.columns[:7]]
+	# drop useless rows
+	df.dropna(axis=0, how='any', inplace=True)
+
+	empty_cols = [df.columns[2], df.columns[5]]
+	df = df[[c for c in df.columns if c not in empty_cols]]
+	df.columns = ["form", "price_pound", "yield", "cup_equiv", "price_cup"]
+
+	df.loc[:, "form"] = df.form.apply(lambda x: ''.join([c for c in str(x) if not c.isdigit()]))
+	df.loc[:, "form"] = df.form.apply(lambda x: condense_form_label(pattern.sub('', x)))
+
+	final_data.append({
+		"name": prod_name, 
+		"data": df.to_dict(orient='records')
+	})
+
+# print to file
+fout_name = "fruits_veggies.json"
+out_path = os.path.abspath(join(curr_path, "..", "data", fout_name))
+f_out = open(out_path, 'w')
+f_out.write(json.dumps(final_data, indent=4, separators=(',', ': ')))
