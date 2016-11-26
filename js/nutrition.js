42335dabc0ab6ebb5bd8a68e01f1574ff91e2455
Samuel Cheng <samuelcheng@college.harvard.edu>
2016-11-26 15:31:41 +0000

    renamed main.js to prevent merge conflicts

--- js/nutrition.js
+++ js/nutrition.js
@@ -22,15 +22,18 @@ var testRecipe =
     };
 
 $(function() {
-    console.log("Ready")
+    console.log("Ready");
+    recipeSearch("chicken");
 });
 
 function nutritionAnalysis(recipe) {
     var app_id = "11d4d52e";
     var app_key = "2d7e359cd72a42a1da103d23be96280d";
-    var baseUrl = "https://api.edamam.com/api/nutrition-details";
 
-    var proxyUrl = `http://localhost:5000/proxy/${baseUrl}`;
+    // var proxyUrl = "http://localhost:5000/n";
+    var git = "https://flaskwebproject120161124101015.scm.azurewebsites.net:443/flaskwebproject120161124101015.git";
+    var baseUrl = "http://flaskwebproject120161124101015.azurewebsites.net/n";
+
     var payload = {
         "app_id": app_id,
         "app_key": app_key,
@@ -38,6 +41,22 @@ function nutritionAnalysis(recipe) {
     };
 
     $.post(proxyUrl, payload, function(data, status) {
+        console.log(data);
+        console.log(status);
+    });
+}
+
+function recipeSearch(query) {
+    var app_id = "58b4a2d1";
+    var app_key = "6632220a737740e2eeb51be026a62788";
+
+    // var baseUrl = "http://edamamproxy.azurewebsites.net/r";
+    var baseUrl = "http://flaskwebproject120161124101015.azurewebsites.net/r";
+    // var baseUrl = "http://localhost:5000/r";
+
+    var proxyUrl = baseUrl + `?q=${query}&app_id=${app_id}&app_key=${app_key}`;
+
+    $.get(proxyUrl, function(data, status) {
         console.log(data);
         console.log(status);
     });
